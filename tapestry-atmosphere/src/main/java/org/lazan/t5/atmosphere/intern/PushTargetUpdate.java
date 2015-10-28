7b3ea65475c71dc3690159190805fb34eae248b9
Jérémie Vendrin <jvendrin@doyoudreamup.com>
2015-10-28 18:04:14 +0100

    Fix call script order

--- tapestry-atmosphere/src/main/java/org/lazan/t5/atmosphere/intern/PushTargetUpdate.java
+++ tapestry-atmosphere/src/main/java/org/lazan/t5/atmosphere/intern/PushTargetUpdate.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.apache.tapestry5.json.JSONArray;
 import org.apache.tapestry5.json.JSONObject;
+import org.apache.tapestry5.services.javascript.InitializationPriority;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
 import org.lazan.t5.atmosphere.model.ContainerModel;
 import org.lazan.t5.atmosphere.model.PushTargetModel;
@@ -39,6 +40,6 @@ public class PushTargetUpdate implements Runnable, ContainerModel {
             targetConfig.put("update", pushTarget.getUpdateStrategy().name());
             config.put(targetConfig);
         }
-        javaScriptSupport.addScript("$('.at-container')[0].atmContainer.updatePushTarget(JSON.parse('%s'))",config.toCompactString());
+        javaScriptSupport.addScript(InitializationPriority.LATE,"$('.at-container')[0].atmContainer.updatePushTarget(JSON.parse('%s'))",config.toCompactString());
     }
 }
